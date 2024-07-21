733330efb97324f477c47c43598c7c3677647421
Roman Janusz <romeqjanoosh@gmail.com>
2024-07-21 02:15:58 +0200

    help

--- client/src/main/scala/com/github/ghik/scadesh/client/ReplClient.scala
+++ client/src/main/scala/com/github/ghik/scadesh/client/ReplClient.scala
@@ -3,7 +3,7 @@ package client
 
 import com.github.ghik.scadesh.core.CommonDefaults
 import com.github.ghik.scadesh.core.utils.PkiUtils
-import org.apache.commons.cli.{DefaultParser, Options, Option as CliOption}
+import org.apache.commons.cli.{DefaultParser, HelpFormatter, Options, Option as CliOption}
 
 import java.net.Socket
 import java.security.PrivateKey
@@ -18,20 +18,23 @@ object ReplClient {
       .converter(Integer.parseInt)
       .desc("port to connect to").build
 
-    val noTls: CliOption = CliOption.builder().longOpt("no-tls")
+    val noTls: CliOption = CliOption.builder.longOpt("no-tls")
       .desc("use plain TCP").build
 
-    val cacert: CliOption = CliOption.builder().longOpt("cacert").hasArg
+    val cacert: CliOption = CliOption.builder.longOpt("cacert").hasArg
       .converter(PkiUtils.loadPemCert)
       .desc("path to CA certificate PEM file").build
 
-    val cert: CliOption = CliOption.builder().longOpt("cert").hasArg
+    val cert: CliOption = CliOption.builder.longOpt("cert").hasArg
       .converter(PkiUtils.loadPemCert)
       .desc("path to client certificate PEM file").build
 
-    val key: CliOption = CliOption.builder().longOpt("key").hasArg
+    val key: CliOption = CliOption.builder.longOpt("key").hasArg
       .converter(PkiUtils.loadPemKey)
       .desc("path to client private key PEM file").build
+
+    val help: CliOption = CliOption.builder.longOpt("help")
+      .desc("show this help").build
   }
 
   private val options = new Options()
@@ -41,28 +44,33 @@ object ReplClient {
     .addOption(Options.cacert)
     .addOption(Options.cert)
     .addOption(Options.key)
+    .addOption(Options.help)
 
 
   def main(args: Array[String]): Unit = {
     val cmdLine = new DefaultParser().parse(options, args)
 
-    val noTls = cmdLine.hasOption(Options.noTls)
-    val host = cmdLine.getOptionValue(Options.host, CommonDefaults.DefaultAddress)
-    val port = cmdLine.getParsedOptionValue[Int](Options.port, CommonDefaults.DefaultPort)
+    if (cmdLine.hasOption(Options.help)) {
+      new HelpFormatter().printHelp("scadesh-client", options)
+    } else {
+      val noTls = cmdLine.hasOption(Options.noTls)
+      val host = cmdLine.getOptionValue(Options.host, CommonDefaults.DefaultAddress)
+      val port = cmdLine.getParsedOptionValue[Int](Options.port, CommonDefaults.DefaultPort)
 
-    val socket =
-      if (noTls) new Socket(host, port)
-      else {
-        val cacert = cmdLine.getParsedOptionValue[X509Certificate](Options.cacert)
-        val cert = cmdLine.getParsedOptionValue[X509Certificate](Options.cert)
-        val key = cmdLine.getParsedOptionValue[PrivateKey](Options.key)
-        val sslContext = PkiUtils.sslContext(
-          PkiUtils.keyManagers(if (cert != null && key != null) Map("key" -> (cert, key)) else Map.empty),
-          PkiUtils.trustManagers(if (cacert != null) Map("cacert" -> cacert) else Map.empty),
-        )
-        sslContext.getSocketFactory.createSocket(host, port)
-      }
+      val socket =
+        if (noTls) new Socket(host, port)
+        else {
+          val cacert = cmdLine.getParsedOptionValue[X509Certificate](Options.cacert)
+          val cert = cmdLine.getParsedOptionValue[X509Certificate](Options.cert)
+          val key = cmdLine.getParsedOptionValue[PrivateKey](Options.key)
+          val sslContext = PkiUtils.sslContext(
+            PkiUtils.keyManagers(if (cert != null && key != null) Map("key" -> (cert, key)) else Map.empty),
+            PkiUtils.trustManagers(if (cacert != null) Map("cacert" -> cacert) else Map.empty),
+          )
+          sslContext.getSocketFactory.createSocket(host, port)
+        }
 
-    TerminalRunner.run(socket)
+      TerminalRunner.run(socket)
+    }
   }
 }
